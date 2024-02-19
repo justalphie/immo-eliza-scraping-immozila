2553203b438ffb77e6a2a8de3b6901e9dde06076
Nachtje <datanerd07@gmail.com>
2024-02-19 14:23:15 +0100

    3 fields fetched from a site

--- scraper/nath_fetch_data_from_site.py
+++ scraper/nath_fetch_data_from_site.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import json
 
 # starting from one URL
+#url = "https://www.immoweb.be/nl/zoekertje/benedenverdieping/te-koop/antwerpen/2100/11150891"
 url = "https://www.immoweb.be/nl/zoekertje/appartement/te-koop/perwijs/1360/11144723"
 r = requests.get(url)
 content = r.content
@@ -32,5 +33,57 @@ json_house_dict = json.dumps(house_dict, indent=4)
 # look at the full json
 print(json_house_dict)
 
-# pick one key from the json and print it's value
-print(house_dict['property']['type'])
+# needed columns
+
+# property ID
+#print(house_dict['id'])
+
+# locality name
+# print(house_dict['property']['location']['locality'])
+
+# # Postal code
+# print(house_dict['property']['location']['postalCode'])
+
+# Price
+
+
+# Type of property (house or apartment)
+
+
+# Subtype of property (bungalow, chalet, mansion, ...)
+
+
+# Type of sale (note: exclude life sales)
+
+
+# Number of rooms
+
+
+# Living area (area in m²)
+
+
+# Equipped kitchen (0/1)
+
+
+# Furnished (0/1)
+
+
+# Open fire (0/1)
+
+
+# Terrace (area in m² or null if no terrace)
+
+
+# Garden (area in m² or null if no garden)
+
+
+# Surface of good
+
+
+# Number of facades
+
+
+# Swimming pool (0/1)
+
+
+# State of building (new, to be renovated, ...)
