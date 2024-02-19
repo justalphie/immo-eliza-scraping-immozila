de1b1b8deac270034e4d3aa85e8878a36a274e86
semdeleersnijder <sem.deleersnijder@outlook.com>
2024-02-19 19:44:57 +0100

    Added immolinks final get links

---
+++ scraper/immolinks.py
@@ -0,0 +1,34 @@
+import requests
+from requests import Session
+from bs4 import BeautifulSoup
+import json
+import re
+import csv
+    
+def immolinks(n):
+    links=[]
+    weblinks=[]
+    teller = 0
+    root_url = "https://www.immoweb.be/nl/zoeken/huis/te-koop?countries=BE&page="
+    for number in range(1, n+1):
+        pages_url = f"{root_url}{number}&orderBy=relevance"
+        r = requests.get(pages_url)
+        print(pages_url, r.status_code)
+        soup = BeautifulSoup(r.content, "html.parser")
+        for contentmain in soup.find_all("div",{"class":"container-main-content"}):
+            for a in contentmain.find_all("a", {"class":"card__title-link"}):
+                    links.append(a)
+        for id, i in enumerate(links):
+            text = str(i)
+            pattern = r'href="([^"]*)"'
+            match = re.search(pattern, text)
+            if match:
+                result = match.group(1)
+                weblinks.append(result)
+                teller += 1
+    print(weblinks)
+    print(teller)
+    return weblinks
+
+if __name__ == "__main__":
+    immolinks(3)
