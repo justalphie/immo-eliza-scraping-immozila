45848e7ba2f78aaa636226da0b497cdbfa3f284c
semdeleersnijder <sem.deleersnijder@outlook.com>
2024-02-20 11:56:11 +0100

    added uniek parameter to immolinkall and add more write_sjon

--- scraper/immolinks.py
+++ scraper/immolinks.py
@@ -3,13 +3,14 @@ from bs4 import BeautifulSoup
 import json
 import re
     
-def immolinks(n):
+def immolinksall(n):
     """
     Starting from a root_url fetches the immoweb links for n pages
     """
-    links=[]
     weblinks=[]
     root_url = "https://www.immoweb.be/nl/zoeken/huis/te-koop?countries=BE&page="
+
+    seen_links = set()
 
     for number in range(1, n+1):
         pages_url = f"{root_url}{number}&orderBy=relevance"
@@ -17,24 +18,20 @@ def immolinks(n):
         print(pages_url, r.status_code)
         soup = BeautifulSoup(r.content, "html.parser")
 
-        for contentmain in soup.find_all("div",{"class":"container-main-content"}):
-
-            for a in contentmain.find_all("a", {"class":"card__title-link"}):
-                    links.append(a)
-        
-        for id, i in enumerate(links):
-            text = str(i)
-            pattern = r'href="([^"]*)"'
-            match = re.search(pattern, text)
-            if match:
-                result = match.group(1)
-                weblinks.append(result)
+        for contentmain in soup.find_all("div", {"class": "container-main-content"}):
+            for a in contentmain.find_all("a", {"class": "card__title-link"}):
+                link = a.get('href')
+                if link and link not in seen_links:
+                    seen_links.add(link)
+                    weblinks.append(link)
     
     print(weblinks)
 
     # number of weblinks fetched from the pages
     print(len(weblinks))
     write_json(weblinks)
+    write_json_houses(weblinks)
+    write_json_appartment(weblinks)
     
     return weblinks
 
@@ -46,5 +43,17 @@ def write_json(weblinks):
     with open("./data/weblinksimmo.json", 'w') as output_file:
         print(json.dumps(weblinks, indent=2), file=output_file)
 
+def write_json_houses(weblinks):
+    filtered_links = [link for link in weblinks if "huis" in link.lower() or "villa" in link.lower() or "herenhuis" in link.lower()]
+
+    with open("./data/weblinksimmohouse.json", 'w') as output_file:
+        json.dump(filtered_links, output_file, indent=2)
+
+def write_json_appartment(weblinks):
+    filtered_links = [link for link in weblinks if "appartement" in link.lower() or "studio" in link.lower()]
+
+    with open("./data/weblinksimmoappartment.json", 'w') as output_file:
+        json.dump(filtered_links, output_file, indent=2)
+
 if __name__ == "__main__":
-    immolinks(50)
+    immolinksall(3)
