657e32c85c846e6a5a7f678a14d0013a73242db9
Nachtje <datanerd07@gmail.com>
2024-02-21 09:53:33 +0100

    cleanup development branch

--- scraper/scraper.py
+++
@@ -1,9 +0,0 @@
-import time
-import requests
-import re
-import json
-import pandas as pd
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from bs4 import BeautifulSoup
